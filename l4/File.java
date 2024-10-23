fb3b853e7512c20333d13070eb5e0f31267fdea9
Enilesi <miriam.iseline@gmail.com>
2024-10-23 15:45:54 +0300

    add class folder

--- l4/File.java
+++ l4/File.java
@@ -1,18 +1,67 @@
 public class File {
     private String information;
     private String name;
-    public File(String information, String name){
-        this.information=information;
-        this.name=name;
+
+    public File(String information, String name) {
+        this.information = information;
+        this.name = name;
     }
-    public String getContent(){
+
+    public String getContent() {
         return this.information;
     }
-};
 
-class MainClass{
-    public static void main(String[] args){
-        File f1=new File("Ana are 10 mere","f1");
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof File) {
+            File other = (File) o;
+            return other.information.equals(this.information) && other.name.equals(this.name);
+        }
+        return false;
+    }
+}
+
+class Folder {
+    private File[] entries = new File[0]; 
+
+    public boolean existsFile(File givenFile) {
+
+        for (File entry:entries) {
+            if (entry.equals(givenFile)) return true;
+        }
+        return false;
+    }
+
+    public boolean addFile(File fileToAdd) {
+        if (existsFile(fileToAdd)) {
+            return false;
+        }
+        File [] newentry= new File[entries.length+1];
+        System.arraycopy(entries, 0, newentry, 0, entries.length);
+        newentry[entries.length] = fileToAdd;
+        entries=newentry;
+        return true;
+    }
+
+    public String getContent() {
+        String concating="";
+        for (File entry:entries) {
+            concating+="\n";
+            concating += entry.getContent();
+        }
+        return concating.toString();
+    }
+}
+
+class MainClass {
+    public static void main(String[] args) {
+        File f1 = new File("Ana are 10 mere", "f1.txt");
+        File f2 = new File("Cana are 10 mere", "f2.txt");
         System.out.println(f1.getContent());
+
+        Folder myFolder = new Folder();
+        myFolder.addFile(f1);
+        myFolder.addFile(f2);
+        System.out.println(myFolder.getContent());
     }
 }
